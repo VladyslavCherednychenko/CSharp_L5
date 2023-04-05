1c54ee94c92fbebc269c57f7923fa59ede1e6a5b
Vladyslav Cherednychenko <vladislav.cherednychenko@gmail.com>
2023-04-06 01:06:42 +0300

    delete garbage

--- CSharp_L5/MainWindow.xaml.cs
+++ CSharp_L5/MainWindow.xaml.cs
@@ -4,22 +4,9 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 using System.Xml.Serialization;
-using static CSharp_L5.MainWindow;
 
 namespace CSharp_L5
 {
